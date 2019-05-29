dde92c28713c73e6c087d755884b3bb78bb06e76
Gina Kim <kimjuk1230@gmail.com>
2019-05-28 17:45:09 -0700

    created navbar

---
+++ christine/src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    <b-navbar toggleable="lg" variant="custom">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+            <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template slot="button-content"><em>User</em></template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+        </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
